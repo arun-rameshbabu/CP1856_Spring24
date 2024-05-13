f1bb286d3fecb78c7a078041b310c82dd6bfc6b5
AustinPerry88 <austin.perry88@ed.cna.nl.ca>
2024-05-13 17:21:52 -0230

    End of Class 2
    End of Class 2

---
+++ Class2/Class2_Gathering_Input.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 16:39:27 2024
+
+@author: Austin
+"""
+
+# test = 10
+# str_test = str(test)
+
+# print("I am going to put " + str(test))
+
+# str_number = '35.5'
+# int_number = int(str_number)
+# flt_number = float(str_number)
+
+# sales_total = int(input("Enter sales total before tax: "))
+# tax_rate = float(input("Enter tax rate: " )) / 100
+# sales_tax = sales_total * tax_rate
+
+# print("The sales tax is: ", end='')
+# print(sales_tax, end= '$200\n')
+
+# print("The sales tax is: $ {}".format(sales_tax))
+
+# print(f"The sales tax is: $ {sales_tax}")
+# 200
+
+side_a = int(input("What is the length of side a? "))
+side_b = int(input("What is the length of side b? "))
+
+area = side_a * side_b
+
+print(f"The area of the rectangle is: {area}")
