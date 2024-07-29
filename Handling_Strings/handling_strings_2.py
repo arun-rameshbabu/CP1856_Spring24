81618ef467aebce248a860690e163f68b71b2850
arun-rameshbabu <arun.rameshbabu@cna.nl.ca>
2024-07-29 16:19:45 -0230

    Week 12 Class 03 interim commit
    String methods complete

---
+++ Handling_Strings/handling_strings_2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 16:10:15 2024
+
+@author: Arun.Rameshbabu
+"""
+
+some_text = 'Some celebrity cameos were already teased or spoiled pre-release, but others may come as complete shocks to fans.'
+
+word_space_split = some_text.split()
