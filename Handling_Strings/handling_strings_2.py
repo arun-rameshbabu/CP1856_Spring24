61a5d8625e43c1f1480dd02462c853cdd17e5b8b
arun-rameshbabu <arun.rameshbabu@cna.nl.ca>
2024-07-29 16:20:24 -0230

    Update handling_strings_2.py

--- Handling_Strings/handling_strings_2.py
+++ Handling_Strings/handling_strings_2.py
@@ -8,3 +8,10 @@ Created on Mon Jul 29 16:10:15 2024
 some_text = 'Some celebrity cameos were already teased or spoiled pre-release, but others may come as complete shocks to fans.'
 
 word_space_split = some_text.split()
+word_space_split[0]
+
+word_comma_split = some_text.split(',')
+
+word_space_split[0] + '|' +  word_comma_split[1]
+
+'|'.join(word_space_split)
